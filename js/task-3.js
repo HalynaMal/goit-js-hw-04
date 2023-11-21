c21b2ae7a18d053277196a29dee1366daabb3d50
HalynaMal <142045196+HalynaMal@users.noreply.github.com>
2023-11-21 18:37:10 +0200

    add task

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,28 @@
-function filterArray(numbers, value) {
-    const array = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-          array.push(numbers[i]) }
-      }
-    return array;
+const profile = {
+  username: "Jacob",
+playTime: 300,
+
+// Метод для зміни імені користувача
+changeUsername: function(newName) {
+  this.username = newName;
+},
+
+// Метод для оновлення кількості годин гри
+updatePlayTime: function(hours) {
+  this.playTime += hours;
+},
+
+// Метод для отримання інформації про профіль
+getInfo: function() {
+  return `${this.username} has ${this.playTime} active hours!`;
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
